585560eb52d2ce733a4831ebb331b3c3930db105
Maxence <max.verdom@gmail.com>
2023-06-22 19:00:34 +0200

    modif main.tf, supp premieres lignes terraform{ ... }

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "3.5.0"
-    }
-  }
-}
-
 provider "google" {
   project     = var.gcp_project
   region      = var.gcp_region
