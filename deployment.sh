849b55a6c72914b231ca293d58f32688afdfdcc9
Maxence <max.verdom@gmail.com>
2023-06-22 21:45:57 +0200

    modif deployment.sh, modif cmd ansible-playbook

--- deployment.sh
+++ deployment.sh
@@ -44,7 +44,7 @@ if [ ! -d "ansible" ]; then
     git clone https://github.com/tp1-deployment-wordpress-gcp.git
     cd tp1-deployment-wordpress-gcp/ansible
 fi
-ansible-playbook -i ansible/playbook.yml
+ansible-playbook -i ansible/ playbook playbook.yml
 
 # 10- Vérification que l'application fonctionne
 wordpress_ip=$(terraform output -raw wordpress_instance_ip)
