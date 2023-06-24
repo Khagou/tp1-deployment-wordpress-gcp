96b5c016825be4b5b618e62070f7898504a49eed
Maxence <max.verdom@gmail.com>
2023-06-24 16:02:08 +0200

    test sur autre projet et autre compte

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ data "google_client_openid_userinfo" "me" {
 
 }
 
-output "my-email" {  value = data.google_client_openid_userinfo.me.email}
+output "my-email" {  value = data.google_client_openid_userinfo.provider_identity.email}
 
 # resource "google_compute_firewall" "allow-ssh" {
 #   name    = "allow-ssh"
