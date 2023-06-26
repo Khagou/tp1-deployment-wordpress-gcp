09b135b95b7293074e7cfe07c4e9054909e7567b
Maxence <max.verdom@gmail.com>
2023-06-26 15:26:44 +0200

    test sur autre projet et autre compte, modif .cfg et .yml

--- deployment.sh
+++ deployment.sh
@@ -24,8 +24,8 @@ if ! command -v ansible &> /dev/null; then
     sudo apt install -y ansible
 fi
 
-gcloud services enable cloudresourcemanager.googleapis.com --project="test-tp1-wordpress-mariadb"
-gcloud services enable iam.googleapis.com --project="test-tp1-wordpress-mariadb"
+gcloud services enable cloudresourcemanager.googleapis.com --project="test-recup-email"
+gcloud services enable iam.googleapis.com --project="test-recup-email"
 
 # 7- Vérification de la présence des fichiers Terraform et exécution de terraform init si nécessaire
 if [ ! -d "terraform" ]; then
