be0d3bfb1525b156570021dab20e188b39ad4797
Maxence <max.verdom@gmail.com>
2023-07-12 15:21:19 +0200

    connexion ssh vm deploiement et lancement script de deploiement

--- vm-deploiement.sh
+++ vm-deploiement.sh
@@ -14,7 +14,7 @@ gcloud compute instances create $INSTANCE_NAME \
   --create-disk=auto-delete=yes,boot=yes,device-name=vm-deploiement,image=projects/debian-cloud/global/images/debian-11-bullseye-v20230629,mode=rw,size=10,type=projects/test-recup-email/zones/us-east1-b/diskTypes/pd-balanced
 
 # Générer les clés SSH
-sudo gcloud compute config-ssh
+sudo gcloud compute config-ssh --project $PROJECT_ID
 
 # Afficher les détails de la VM créée
 gcloud compute instances describe $INSTANCE_NAME \
@@ -26,3 +26,13 @@ gcloud compute ssh $INSTANCE_NAME \
   --project $PROJECT_ID \
   --zone $ZONE \
   --command "if ! command -v git &> /dev/null; then sudo apt-get update && sudo apt-get install -y git; fi"
+
+gcloud compute ssh $INSTANCE_NAME \
+  --project $PROJECT_ID \
+  --zone $ZONE \
+  --command "if [ ! -d "tp1-deployment-wordpress-gcp" ]; then git clone https://github.com/tp1-deployment-wordpress-gcp.git
+    cd tp1-deployment-wordpress-gcp
+    else
+        cd tp1-deployment-wordpress-gcp
+    fi
+    sh deployment.sh"
