d2a6cf38c13fdb0f35031bfd98204b6631186a20
Maxence <max.verdom@gmail.com>
2023-07-19 15:41:57 +0200

    depart 0 new project

--- test_export_from_file.sh
+++ test_export_from_file.sh
@@ -3,7 +3,7 @@ export USER=test
 export EMAIL=khagukhagu2@gmail.com
 # 4- Vérifier si une clé SSH est présente sur la VM, sinon en créer une
 if [ ! -f ~/.ssh/id_rsa ]; then
-    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa -C "$USER"
+    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa -C "$USER" -b 2048
 fi
 
 # Lire le contenu du fichier dans une variable
