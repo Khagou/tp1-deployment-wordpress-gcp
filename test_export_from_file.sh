3041ad18a33e3847443919665825b3d459c1ee3c
Maxence <max.verdom@gmail.com>
2023-07-20 10:11:02 +0200

    depart 0 new project

--- test_export_from_file.sh
+++ test_export_from_file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export EMAIL=khagukhagu2 #Remplacer par votre Email
+export USER=khagukhagu2 #Remplacer par votre user name
 # 4- Vérifier si une clé SSH est présente sur la VM, sinon en créer une
 if [ ! -f ~/.ssh/id_rsa ]; then
-    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa -C "$EMAIL"
+    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa -C "$USER"
 fi
 
 # Lire le contenu du fichier dans une variable
@@ -12,4 +12,4 @@ contenu=$(cat ~/.ssh/id_rsa.pub)
 export VARIABLE_CONTENU="$contenu"
 
 # Afficher le contenu exporté
-echo "$EMAIL:$VARIABLE_CONTENU" > ssh_keys
+echo "$USER:$VARIABLE_CONTENU" > ssh_keys
