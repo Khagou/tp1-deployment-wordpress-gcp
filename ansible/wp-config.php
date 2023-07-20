0a78b8b3ca8e98fd56f9bdcbf1f9652afe94a253
Maxence <max.verdom@gmail.com>
2023-07-20 20:49:42 +0200

    modif playbook,

--- ansible/wp-config.php
+++ ansible/wp-config.php
@@ -1,10 +1,10 @@
 <?php
-define( 'DB_NAME', '{{ wordpress_db_name }}' );
-define( 'DB_USER', '{{ wordpress_db_user }}' );
-define( 'DB_PASSWORD', '{{ wordpress_db_password }}' );
-define( 'DB_HOST', '{{ mariadb_instance_ip }}' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
+define('DB_NAME', 'wordpressdb');
+define('DB_USER', 'wordpressuser');
+define('DB_PASSWORD', 'wordpresspassword');
+define('DB_HOST', '34.75.197.142');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
 
 // define( 'AUTH_KEY',         'put your unique phrase here' );
 // define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
@@ -13,11 +13,3 @@ define( 'DB_COLLATE', '' );
 // define( 'AUTH_SALT',        'put your unique phrase here' );
 // define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 // define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-
-$table_prefix = 'wp_';
-
-define( 'WP_DEBUG', false );
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
-}
-require_once ABSPATH . 'wp-settings.php';
