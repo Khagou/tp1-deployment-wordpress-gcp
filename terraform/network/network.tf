2204cc5d0b9edd479acc89a8ed91a7c5c86cb160
Maxence <max.verdom@gmail.com>
2023-07-21 17:08:03 +0200

    modif playbook,

--- terraform/network/network.tf
+++ terraform/network/network.tf
@@ -35,6 +35,9 @@ resource "google_compute_subnetwork" "my_subnetwork" {
 output "network_name" {
   value = google_compute_network.my_network.name
 }
+output "network_self_link" {
+  value = google_compute_network.my_network.self_link
+}
 
 output "subnet_name" {
   value = google_compute_subnetwork.my_subnetwork.name
