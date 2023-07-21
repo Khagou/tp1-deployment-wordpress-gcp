fc3e8b5cafef4223f1afb67c6260634e59017955
Maxence <max.verdom@gmail.com>
2023-07-21 17:06:02 +0200

    modif playbook,

--- terraform/network/outputs.tf
+++ terraform/network/outputs.tf
@@ -1,6 +1,6 @@
-output "subnet_self_link" {
-  value = module.my_subnetwork.self_link
-}
-output "network_self_link" {
-  value = module.my_subnetwork.self_link
-}
+# output "subnet_self_link" {
+#   value = module.my_subnetwork.self_link
+# }
+# output "network_self_link" {
+#   value = module.my_subnetwork.self_link
+# }
