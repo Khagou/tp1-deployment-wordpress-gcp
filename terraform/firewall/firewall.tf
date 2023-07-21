b5fc8b18201b8f8a261bbc138465fb241e283276
Maxence <max.verdom@gmail.com>
2023-07-21 14:59:39 +0200

    modif playbook,

--- terraform/firewall/firewall.tf
+++ terraform/firewall/firewall.tf
@@ -1,13 +1,13 @@
 # firewall/firewall.tf
 
-# variable "network_self_link" {
-#   description = "Self link of the Google Cloud network."
-# }
+variable "network_self_link" {
+  description = "Self link of the Google Cloud network."
+}
 
-# variable "subnet_cidr" {
-#   description = "CIDR block for the subnetwork."
-#   default = "10.0.0.0/24"
-# }
+variable "subnet_cidr" {
+  description = "CIDR block for the subnetwork."
+  default = "10.0.0.0/24"
+}
 
 provider "google" {
   region = var.region
