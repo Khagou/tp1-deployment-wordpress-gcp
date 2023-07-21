cc06a5e2f0fff6af2181a8a0433167fbb5a2ceee
Maxence <max.verdom@gmail.com>
2023-07-21 14:13:01 +0200

    modif playbook,

--- deployment.sh
+++ deployment.sh
@@ -59,8 +59,8 @@ url_to_check="$wordpress_ip/wordpress"  # Remplacez cette URL par l'URL que vous
 # Effectuer une requête GET à l'URL spécifiée et stocker le code de statut dans une variable
 status_code=$(curl -s -o /dev/null -w "%{http_code}" $url_to_check)
 
-if [ $status_code -eq 200 ] || [ $status_code -eq 301 ]; then
-  echo "La page $url_to_check renvoie le code 200."
+if [ $status_code -eq 200 ] || [ $status_code -eq 301 ] || [ $status_code -eq 302 ]; then
+  echo "La page $url_to_check renvoie le code 200. Code : $status_code"
 else
   echo "La page $url_to_check ne renvoie pas le code 200. Code : $status_code"
 fi
