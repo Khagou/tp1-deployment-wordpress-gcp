ed622973ced1477acd722a57fafe3ea02f36d05b
Maxence <max.verdom@gmail.com>
2023-07-21 13:59:00 +0200

    modif playbook,

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export ZONE=us-east1-b # A modifier
 export WORDPRESS_INSTANCE=wordpress-instance # A modifier
-
 # 5- Vérifier et installer Terraform si nécessaire
 if ! command -v terraform &> /dev/null; then
     sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
@@ -53,12 +52,12 @@ fi
 ansible-playbook playbook.yml -i ./gcp_compute.yml
 
 # 10- Vérification que l'application fonctionne
-wordpress_ip=$(terraform output -raw wordpress_instance_ip)
+wordpress_ip=$(gcloud compute instances describe $WORDPRESS_INSTANCE --zone $ZONE --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
 
 url_to_check="$wordpress_ip/wordpress"  # Remplacez cette URL par l'URL que vous souhaitez vérifier
 
 # Effectuer une requête GET à l'URL spécifiée et stocker le code de statut dans une variable
-status_code=$(gcloud compute instances describe $WORDPRESS_INSTANCE --zone $ZONE --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+status_code=$(curl -s -o /dev/null -w "%{http_code}" $url_to_check)
 
 if [ $status_code -eq 200 ]; then
   echo "La page $url_to_check renvoie le code 200."
